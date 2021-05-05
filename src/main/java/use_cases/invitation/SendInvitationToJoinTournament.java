afd06602557b3e72da5f1a2847d62fdb564f2158
Thomas FERREIRA <thomas_ferreira_93330@hotmail.fr>
2021-05-05 18:15:14 +0200

    Add new use_case

--- src/main/java/use_cases/invitation/SendInvitationToJoinTournament.java
+++ src/main/java/use_cases/invitation/SendInvitationToJoinTournament.java
@@ -1,4 +1,39 @@
 package use_cases.invitation;
 
+import model.Invitation;
+import model.Team;
+import model.Tournament;
+import model.User;
+
+import java.util.Optional;
+
 public class SendInvitationToJoinTournament {
+    public Invitation SendInvitation(
+            String tournamentId,
+            String receiverId,
+            Enum receiverType,
+            String senderId,
+            Optional<String> invitationMessage
+    ) {
+        Invitation invitation = new Invitation();
+        Tournament tournament = TournamentRepository.getTournament(tournamentId);
+        invitation.setTournament(tournament);
+
+        if (receiverType == Enum.TEAM) {
+            Team team = TeamRepository.getTeam(receiverId);
+            invitation.setReceiver(team);
+        } else {
+            User user = UserRepository.getUser(receiverId);
+            invitation.setReceiver(user);
+        }
+
+        User sender = UserRepository.getUser(senderId);
+        invitation.setSender(sender);
+
+        if (!invitationMessage.isPresent()) invitation.setMessage(invitationMessage.get());
+
+        invitation.send();
+
+        return invitation;
+    }
 }
--- src/main/java/use_cases/tournament/CreateTournament.java
+++ src/main/java/use_cases/tournament/CreateTournament.java
@@ -3,12 +3,13 @@ package use_cases.tournament;
 import model.Tournament;
 
 public class CreateTournament {
-    public Tournament initiate(int nbTeams, String tournamentName, LocalDate startDate, LocalDate endDate, User admin){
+    public Tournament initiate(int nbTeams, int teamSize, String tournamentName, LocalDate startDate, LocalDate endDate, User admin){
         Tournament tournament = new Tournament();
         tournament.setAdmin(admin);
         tournament.setStartDate(startDate);
         tournament.setEndDate(endDate);
         tournament.setNbTeam(nbTeams);
+        tournament.setTeamSize(teamSize);
         tournament.save();
 
         return tournament;
